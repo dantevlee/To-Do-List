9e3a665edfe70f715c69a455ed66e6fff2e9ff31
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2023-09-19 22:48:13 -0500

    added local storage

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
 const formElement = document.querySelector(".toDoForm");
-const buttonElement = document.querySelector(".submitButton");
 const inputElement = document.querySelector(".inputToDo");
 const ulEl = document.querySelector(".list");
 
-buttonElement.addEventListener("click", (event) => {
+// Initialize the list from local storage or create an empty array
+let list = JSON.parse(localStorage.getItem("list")) || [];
+
+// Function to update local storage with the current list
+function updateLocalStorage() {
+  localStorage.setItem("list", JSON.stringify(list));
+}
+
+function createToDoList(newToDo) {
+  const newLine = document.createElement("div");
+  newLine.classList.add("task");
+  
+  const textSpan = document.createElement("span");
+  textSpan.classList.add("text");
+  textSpan.textContent = newToDo;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete-button");
+  deleteButton.textContent = "X";
+
+  deleteButton.addEventListener("click", function () {
+    ulEl.removeChild(newLine);
+    // Remove the task from the list
+    const index = list.indexOf(newToDo);
+    if (index !== -1) {
+      list.splice(index, 1);
+      updateLocalStorage(); // Update local storage
+    }
+  });
+
+  ulEl.appendChild(newLine);
+  newLine.appendChild(textSpan);
+  newLine.appendChild(deleteButton);
+
+}
+
+function clearInputField() {
+  inputElement.value = "";
+}
+
+formElement.addEventListener("submit", (event) => {
   event.preventDefault();
   let newToDo = inputElement.value;
   if (newToDo.trim() !== "") {
-    const newLine = document.createElement("div");
-    newLine.classList.add("task");
-    newLine.innerHTML = `
-    <span class="text">${newToDo}</span>
-    <button class="delete-button">X</button>
-`;
-
-    const deleteButton = newLine.querySelector(".delete-button");
-    deleteButton.addEventListener("click", function () {
-      ulEl.removeChild(newLine);
-    });
-
-    ulEl.appendChild(newLine)
-
-    inputElement.value = ""; // Clear the input field
+    createToDoList(newToDo);
+    list.push(newToDo); // Add the task to the list
+    updateLocalStorage(); // Update local storage
+    clearInputField(); // Clear the input field
   }
 });
 
-function createToDoList(newToDo) {
-  const deleteButton = document.createElement("button");
-  newToDo.appendChild(deleteButton);
-}
+// Initialize the task list from the saved list
+list.forEach((task) => {
+  createToDoList(task);
+});
